1f2ee2e8c82be452f20fd354564cd7950da4139b
henry.chen <henry.chen@trustasia.com>
2024-05-06 22:29:25 +0800

    fix: UIGraphicsBeginImageContext() failed to allocate CGBitampContext

--- Sources/CarBode/CBBarcodeView.swift
+++ Sources/CarBode/CBBarcodeView.swift
@@ -135,15 +135,11 @@ private extension UIImage {
         newSize.width = floor(newSize.width)
         newSize.height = floor(newSize.height)
 
-        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
-        if let context = UIGraphicsGetCurrentContext() {
-            context.translateBy(x: newSize.width / 2, y: newSize.height / 2)
-            context.rotate(by: CGFloat(radians))
+        let renderer = UIGraphicsImageRenderer(size: newSize)
+        return renderer.image { context in
+            context.cgContext.translateBy(x: newSize.width / 2, y: newSize.height / 2)
+            context.cgContext.rotate(by: CGFloat(radians))
             self.draw(in: CGRect(x: -self.size.width / 2, y: -self.size.height / 2, width: self.size.width, height: self.size.height))
-            let newImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return newImage
         }
-        return nil
     }
 }
